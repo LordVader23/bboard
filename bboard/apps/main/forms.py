a999df86473f783acaa00fe7128471d2c31dfaa0
LordVader23 <58340256+LordVader23@users.noreply.github.com>
2019-12-26 18:07:58 +0300

    Create add_answers form

--- bboard/apps/main/forms.py
+++ bboard/apps/main/forms.py
@@ -7,6 +7,7 @@ from .models import user_registrated
 from .models import SuperRubric, SubRubric
 from .models import Bb, AdditionalImage
 from .models import Comment
+from .models import Answers
 
 from captcha.fields import CaptchaField
 
@@ -147,4 +148,11 @@ class CommentChangeForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ('is_active', )
+        widgets = {'bb': forms.HiddenInput, 'author': forms.HiddenInput, }
+
+
+class CommentAddAnswer(forms.ModelForm):
+    class Meta:
+        model = Answers
+        exclude = ('is_active', 'answers',)
         widgets = {'bb': forms.HiddenInput, 'author': forms.HiddenInput, }
