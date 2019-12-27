d9764a1509f1af7eae146e3c32190d41da7fdca6
LordVader23 <58340256+LordVader23@users.noreply.github.com>
2019-12-27 17:32:06 +0300

    Add url for adding answer to answer

--- bboard/apps/main/urls.py
+++ bboard/apps/main/urls.py
@@ -17,6 +17,7 @@ from .views import login
 from .views import comment_change
 from .views import comment_add_answer
 from .views import answer_change
+from .views import answer_add_answer
 
 
 app_name = 'main'
@@ -40,6 +41,7 @@ urlpatterns = [
     path('comment/change/<int:comment_id>/', comment_change, name='comment_change'),
     path('comment/add_answer/<int:comment_id>/', comment_add_answer, name='comment_add_answer'),
     path('answer/change/<int:answer_id>/', answer_change, name='answer_change'),
+    path('answer/add_answer/<int:comment_id>/<int:answer_id>/', answer_add_answer, name='answer_add_answer'),
     path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
     path('<int:pk>/', by_rubric, name='by_rubric'),
     path('<str:page>/', other_page, name='other'),
