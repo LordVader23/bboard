1c5a3727dca6f0d7ae0c83b939bcc01e6cedb616
LordVader23 <58340256+LordVader23@users.noreply.github.com>
2020-01-06 19:22:18 +0300

    Change static settings

--- bboard/settings.py
+++ bboard/settings.py
@@ -132,10 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'bboard/apps/main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     ('main_css', 'D:/Django/bboard/bboard/apps/main/static/css')
 ]
+STATICFILES_FINDERS = (
+
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+)
 
 # Auth user
 AUTH_USER_MODEL = 'main.AdvUser'
