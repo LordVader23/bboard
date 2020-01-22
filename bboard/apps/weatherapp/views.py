5ae48c5eb653d20d39549a05988b16f1db369185
LordVader23 <artem.fedotenko23@gmail.com>
2020-01-22 21:16:35 +0200

    Almost create func index

--- bboard/apps/weatherapp/views.py
+++ bboard/apps/weatherapp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '57725f55f04ae1d78715ff2234f8dfa6'
+    url = 'https://api.openweathermap.org/data/2.5/' \
+          'weather?q={}&units={}&appid={}'
+    temp = 'metric'
+    city = 'London'
+    res = requests.get(url.format(city, temp, appid)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather']['icon']
+    }
+
+    context = {
+        'info': city_info
+    }
+
+    return render(request, 'weather/index.html', city_info)
 
