3a7763aa41ca570823f16a47130fcee030ee385b
LordVader23 <artem.fedotenko23@gmail.com>
2020-01-25 17:28:17 +0200

    Changed

--- bboard/apps/weatherapp/forms.py
+++ bboard/apps/weatherapp/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import City
+from .utilities import temp_queryset
 
 
 class CityForm(forms.ModelForm):
+    # temp = forms.ModelChoiceField(queryset=temp_queryset, label='Температура', empty_label='Цельсий')
+
     class Meta:
         model = City
         fields = ['name']
