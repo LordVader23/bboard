a696a4bfb0278252a38e483cb237abf70015b9cc
LordVader23 <artem.fedotenko23@gmail.com>
2020-02-02 17:57:17 +0200

    Field name is unique now

--- bboard/apps/weatherapp/models.py
+++ bboard/apps/weatherapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class City(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название города')
+    name = models.CharField(max_length=50, verbose_name='Название города', unique=True)
 
     def __str__(self):
         return self.name
